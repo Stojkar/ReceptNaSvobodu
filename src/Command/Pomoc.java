47b7f2f1960575aa8b5717679a212638c787919c
Marek <marekksmid@gmail.com>
2026-02-21 19:39:08 +0100

    vytvoreni ReceptNaSvobodu_jar.xml a lehke upravi

--- src/Command/Pomoc.java
+++ src/Command/Pomoc.java
@@ -1,9 +1,9 @@
 package Command;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Pomoc implements Command {
     @Override
@@ -14,19 +14,19 @@ public class Pomoc implements Command {
     @Override
     public String execute(String smer) {
         StringBuilder pomoc = new StringBuilder();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("pomoc.txt");
 
-        try (BufferedReader reader = new BufferedReader(
-                new FileReader("res/pomoc.txt"))) {
+        if (is == null) {
+            return "Nápověda není dostupná.";
+        }
 
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 pomoc.append(line).append("\n");
             }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            return "Chyba při načítání nápovědy.";
         }
 
         return pomoc.toString();
